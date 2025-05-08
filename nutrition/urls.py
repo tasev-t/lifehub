c03bb0a190af2d2707800ced3373207b7f05c190
tasev-t <tasev.t@gmail.com>
2025-05-08 12:15:51 +0200

    Enable full CRUD on nutrition recipes and update URLs

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
-    RecipeListView,
+    RecipeListCreateView, RecipeDetailView,
     IngredientListCreateView, IngredientDetailView,
     MealPlanListCreateView, MealPlanDetailView,
 )
 
 urlpatterns = [
-    path('recipes/', RecipeListView.as_view(), name='recipe-list'),
+    path('recipes/', RecipeListCreateView.as_view(), name='recipe-list'),
+    path('recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
     path('ingredients/', IngredientListCreateView.as_view(), name='ingredient-list'),
     path('ingredients/<int:pk>/', IngredientDetailView.as_view(), name='ingredient-detail'),
     path('mealplans/', MealPlanListCreateView.as_view(), name='mealplan-list'),
